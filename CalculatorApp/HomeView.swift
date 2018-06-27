ecede10f2bed66579376108bb1b82ae48798fb77
Muhammad Naeem Jawaid <naeemjawaid@gmail.com>
2018-06-27 10:27:41 +0500

    Remove comments in master branch.

--- CalculatorApp/HomeView.swift
+++ CalculatorApp/HomeView.swift
@@ -6,9 +6,6 @@
 //  Copyright © 2018 Swift iOS Tutorials. All rights reserved.
 //
 
-// This line of comment is added in branch: develop
-// this line of comment is added in comment-branch
-
 import UIKit
 
 class HomeView: UIView {
